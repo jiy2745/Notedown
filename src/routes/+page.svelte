59ea40e8fe6bdfa4af72b6564c1f91605b8d87b4
jiy2745 <jiy2745@gmail.com>
2025-04-03 03:02:32 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    let isMemoClicked = false;
+    function memoClick() {
+        isMemoClicked = true;
+    }
+</script>
+
+<div class="memo" on:click={memoClick} class:is-memo-clicked={isMemoClicked}></div>
+
+<style>
+    .memo {
+        width: 100px;
+        height: 100px;
+        background-color: wheat;
+    }
+
+    .is-memo-clicked {
+        background-color: blueviolet;
+    }
+</style>
