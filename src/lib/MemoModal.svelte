c4788fcb76c33001978e24dbacfe0eec2222428e
jiy2745 <jiy2745@gmail.com>
2025-04-09 03:06:35 +0900

    Testing memo

---
+++ src/lib/MemoModal.svelte
@@ -0,0 +1,49 @@
+
+
+<script lang="ts">
+    export let title: string;
+    export let text: string;
+    export let isOpen: boolean;
+
+    function closeMemoModal(): void {
+        isOpen = false;
+    }
+</script>
+
+{#if isOpen}
+<div class="background" on:click={closeMemoModal}>
+    <div class="memo-modal" on:click|stopPropagation>
+        <textarea bind:value={title}></textarea>
+        <textarea bind:value={text}></textarea>
+    </div>
+</div>
+{/if}
+
+<style>
+    textarea {
+        width: 90%;
+        border: none;
+        outline: none;
+        resize: none;
+        margin: 5px;
+    }
+
+    .background {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.5);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .memo-modal {
+        width: 300px;
+        height: 500px;
+        background-color: white;
+        border-radius: 10px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,26 @@
-<script>
-    let isMemoClicked = false;
-    function memoClick() {
-        isMemoClicked = true;
-    }
+<script lang="ts">
+    import MemoModal from "$lib/MemoModal.svelte";
+
+    let title: string = "Memo title";
+    let text: string = "Memo text";
+    let isOpen: boolean = false;
 </script>
 
-<div class="memo" on:click={memoClick} class:is-memo-clicked={isMemoClicked}></div>
+<div class="memo" on:click={() => isOpen = true}>
+    <p>{title}</p>
+    <p>{text}</p>
+</div>
+
+<MemoModal
+    bind:title
+    bind:text
+    bind:isOpen
+/>
 
 <style>
     .memo {
         width: 100px;
         height: 100px;
         background-color: wheat;
-    }
-
-    .is-memo-clicked {
-        background-color: blueviolet;
     }
 </style>
